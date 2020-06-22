19731fb7487fae2cdadb2b4665bd8afdc110fbad
Marsel Garipov <marsel.garipov.rus@gmail.com>
2020-06-22 20:37:42 +0300

    change url

--- PdAppSite/src/app/sqlrequest.service.ts
+++ PdAppSite/src/app/sqlrequest.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { formatDate } from '@angular/common';
 
-const serverUrl = "http://localhost:8050/"
+const serverUrl = "http://майапд.рус/back"
 
 @Injectable({
   providedIn: 'root'
